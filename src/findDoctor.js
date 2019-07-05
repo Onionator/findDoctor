b06a535f3834dd2938a9d768a65be9077f1a7719
Student <example@example.com>
2019-07-05 15:47:01 -0700

    add functionality to the second button and its input.

--- src/findDoctor.js
+++ src/findDoctor.js
@@ -13,14 +13,13 @@ export class DoctorFinder {
 
       };
       request.open('GET', url, true);
-      // request.setRequestHeader('Accept', 'application/json');
       request.send();
     });
   }
-  goAway(word) {
+  doctorInfo(word) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://www.foaas.com/${word}/Sam`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${word}&location=45.515%2C-122.679%2C100&user_location=45.515%2C-122.679&skip=0&limit=10&user_key=${apiKey}`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
@@ -30,7 +29,6 @@ export class DoctorFinder {
 
       };
       request.open('GET', url, true);
-      request.setRequestHeader('Accept', 'application/json');
       request.send();
     });
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { DoctorFinder } from './findDoctor';
 import $ from 'jquery';
 
 $(document).ready(function () {
-  $('.define').click(function () {
-    $(`#doctorsList`).text('');
-    let word = $('#word').val();
-    $('#word').val('');
+  $('.btnSymptoms').click(function () {
+    $(`#doctorsList`).html('');
+    let word = $('#symptoms').val();
+    $('#symptoms').val('');
     let findDoctor = new DoctorFinder();
     let promise = findDoctor.findADoctor(word);
 
@@ -14,11 +14,37 @@ $(document).ready(function () {
       console.log(body);
       for (let i = 0; i < body.data.length; i++) {
         $(`#doctorsList`).append(
-          `<h2>${body.data[i].practices[0].name}</h2>
-          <li>${body.data[i].practices[0].visit_address.street} ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}</li>
-          <li>${parseInt(body.data[i].practices[0].distance)} miles away.</li>
-          <li>Taking new patients: ${body.data[i].practices[0].accepts_new_patients}</li>
-          <li>website: ${body.data[i].practices[0].website}`
+          `<div class="doctorsInfo">
+            <h2>${body.data[i].practices[0].name}</h2>
+            <li>${body.data[i].practices[0].visit_address.street} ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}</li>
+            <li>${parseInt(body.data[i].practices[0].distance)} miles away.</li>
+            <li>Taking new patients: ${body.data[i].practices[0].accepts_new_patients}</li>
+            <li>website: ${body.data[i].practices[0].website}</li>
+          </div>`
+        );
+      }
+    });
+  });
+  $('.btnDoctor').click(function () {
+    $(`#doctorsList`).text('');
+    let word = $('#doctor').val();
+    $('#doctor').val('');
+    let findDoctor = new DoctorFinder();
+    let promise = findDoctor.findADoctor(word);
+
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      console.log(body);
+      for (let i = 0; i < body.data.length; i++) {
+        $(`#doctorsList`).append(
+          `<div class="doctorsInfo">
+            <img src="${body.data[i].profile.image_url}" alt="picture of ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}">
+            <h2>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}</h2>
+            <li>${body.data[i].practices[0].visit_address.street} ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}</li>
+            <li>${parseInt(body.data[i].practices[0].distance)} miles away.</li>
+            <li>Taking new patients: ${body.data[i].practices[0].accepts_new_patients}</li>
+            <li>website: ${body.data[i].practices[0].website}</li>
+          </div>`
         );
       }
     });
