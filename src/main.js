7318a832ff682db386561e20efc4ea25bdca0239
Student <example@example.com>
2019-07-05 15:04:28 -0700

    modify the main.js to display everyoutput in its own list.

--- src/main.js
+++ src/main.js
@@ -3,28 +3,24 @@ import $ from 'jquery';
 
 $(document).ready(function () {
   $('.define').click(function () {
+    $(`#doctorsList`).text('')
     let word = $('#word').val();
     $('#word').val('');
     let findDoctor = new DoctorFinder();
     let promise = findDoctor.findADoctor(word);
-    let promiseTwo = findDoctor.goAway(word);
 
     promise.then(function (response) {
       let body = JSON.parse(response);
       console.log(body);
-      for (let i = 0; i < 3; i++) {
-        $(`#doctorName${i}`).html(body.data[i].practices[0].name);
-        $(`#doctorAddress${i}`).html(`${body.data[i].practices[0].visit_address.street} ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}`);
-        $(`#doctorDistance${i}`).html(`${parseInt(body.data[i].practices[0].distance)} miles away.`);
-        $(`#takingNewPatients${i}`).html(` Taking new patients: ${body.data[i].practices[0].accepts_new_patients}`);
-        $(`#doctorWebsite${i}`).html(body.data[i].practices[0].website);
+      for (let i = 0; i < body.data.length; i++) {
+        $(`#doctorsList`).append(
+          `<h2>${body.data[i].practices[0].name}</h2>
+          <li>${body.data[i].practices[0].visit_address.street} ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip}</li>
+          <li>${parseInt(body.data[i].practices[0].distance)} miles away.</li>
+          <li>Taking new patients: ${body.data[i].practices[0].accepts_new_patients}</li>
+          <li>website: ${body.data[i].practices[0].website}`
+        );
       }
-    });
-
-    promiseTwo.then(function (response) {
-      let body = JSON.parse(response);
-      console.log(body);
-      $('.showInsult').html(body.message);
     });
   });
 });
